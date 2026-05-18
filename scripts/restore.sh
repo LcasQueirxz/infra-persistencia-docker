c37e91e5695e4e3a82132008549a5c01600e4459
Lucas Gabriel <lucascamposqueiroz15@gmail.com>
2026-05-18 23:41:00 +0000

    feat: estrutura final obrigatoria AC2

---
+++ scripts/restore.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Script de restauração de volume
+echo "[INFO] Iniciando restauração..."
+# Exemplo: docker run --rm -v mysql-prod-data:/dest -v $(pwd)/backups:/src alpine tar xzf /src/mysql-backup-ARQUIVO.tar.gz -C /dest
+echo "[SUCESSO] Restauração simulada concluída."
